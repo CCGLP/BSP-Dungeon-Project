fafaee513137c83e2db57c95cee03bc9e5f8a27a
CCGLP <carlosgolfer@gmail.com>
2018-10-15 22:31:38 +0200

    Refactored inited, map generation in 2D without corridors

---
+++ Assets/Code/Legacy/LegacyRoom.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LegacyRoom {
+
+
+    //Starting point from left-down corner
+    private Vector3 startPoint;
+    private Vector3 roomSize;
+    private int numberOfRooms;
+    private float roomPercentage = 0.975f;
+    private List<LegacyRoom> subRooms;
+    private int numberCorridors = 0;
+    //First generation, first number, second, second... 
+    private string generationString;
+    public Vector3 StartPoint
+    {
+        get
+        {
+            return startPoint;
+        }
+
+    }
+
+    public Vector3 RoomSize
+    {
+        get
+        {
+            return roomSize;
+        }
+
+    }
+
+    public List<LegacyRoom> SubRooms
+    {
+        get
+        {
+            return subRooms;
+        }
+
+       
+    }
+
+    public int NumberCorridors
+    {
+        get
+        {
+            return numberCorridors;
+        }
+
+        set
+        {
+            numberCorridors = value;
+        }
+    }
+
+    public string GenerationString
+    {
+        get
+        {
+            return generationString;
+        }
+
+    }
+
+    public LegacyRoom (Vector3 startPoint, Vector3 roomSize, int roomDivisions, float roomPercentage, string previousGeneration = "")
+    {
+        this.startPoint = startPoint;
+        this.roomSize = roomSize;
+        this.numberOfRooms = roomDivisions;
+        this.roomPercentage = roomPercentage;
+        this.generationString = previousGeneration;
+        if (this.numberOfRooms == 1)
+        {
+            this.startPoint = this.startPoint * (1 + (1 - roomPercentage));
+            this.roomSize = this.roomSize * roomPercentage;
+
+        }
+        else if (this.numberOfRooms > 1)
+        {
+            this.subRooms = new List<LegacyRoom>();
+            bool twoD = this.roomSize.z == 0;
+
+            //Re-escalate the room for lesser size. 
+            Vector3 trueStartPoint = new Vector3(this.startPoint.x + 15, twoD ? this.startPoint.y + 15 : 0, twoD ? 0 : this.startPoint.z +15);
+            Vector3 trueRoomSize = new Vector3(this.roomSize.x -15, twoD ? this.roomSize.y -15 : 0, twoD?0:this.roomSize.z -15); //* this.roomPercentage;
+
+            bool horizontal = Random.Range(0, 100) < 50;
+            int firstRooms;
+            if (numberOfRooms > 2)
+            {
+                firstRooms = (int)Random.Range(this.numberOfRooms * 0.4f, this.numberOfRooms * 0.6f);
+            }
+            else
+            {
+                firstRooms = 1;
+            }
+            float sizeFirstRoom;
+            if (horizontal)
+            {
+                
+                sizeFirstRoom = twoD ? trueRoomSize.y : trueRoomSize.z;
+                sizeFirstRoom = Random.Range(sizeFirstRoom * 0.4f, sizeFirstRoom * 0.6f) ;
+                LegacyRoom room = new LegacyRoom(trueStartPoint, new Vector3(trueRoomSize.x, twoD ? sizeFirstRoom : 0, twoD ? 0 : sizeFirstRoom), firstRooms, roomPercentage, this.generationString+"0");
+                this.subRooms.Add(room);
+                if ((this.numberOfRooms - firstRooms) != 0)
+                {
+                    room = new LegacyRoom(trueStartPoint + (twoD ? Vector3.up * (sizeFirstRoom): Vector3.forward * (sizeFirstRoom)),
+                    new Vector3(trueRoomSize.x, twoD ? trueRoomSize.y - sizeFirstRoom : 0, twoD ? 0 : trueRoomSize.z - sizeFirstRoom), this.numberOfRooms - firstRooms, roomPercentage, this.generationString+"1");
+                    this.subRooms.Add(room);
+                }
+            }
+            else
+            {
+                
+                sizeFirstRoom = Random.Range(trueRoomSize.x * 0.4f, trueRoomSize.x * 0.6f);
+                LegacyRoom room = new LegacyRoom(trueStartPoint, new Vector3(sizeFirstRoom, twoD ? trueRoomSize.y : 0, twoD ? 0 : trueRoomSize.z), firstRooms, roomPercentage, this.generationString + "0");
+                this.subRooms.Add(room);
+                if ((this.numberOfRooms - firstRooms) != 0)
+                {
+                    room = new LegacyRoom(trueStartPoint + Vector3.right * (sizeFirstRoom),
+                        new Vector3(trueRoomSize.x - sizeFirstRoom, twoD ? trueRoomSize.y : 0, twoD ? 0 : trueRoomSize.z), this.numberOfRooms - firstRooms, roomPercentage, this.generationString +"1");
+                    this.subRooms.Add(room);
+                }
+            }
+
+        }
+    }
+
+    public List<LegacyRoom> GetSubRooms()
+    {
+        List<LegacyRoom> rooms = new List<LegacyRoom>();
+        if (this.subRooms == null)
+        {
+            rooms.Add(this);
+            return rooms;
+        }
+        else
+        {
+            for (int i = 0; i < this.subRooms.Count; i++)
+            {
+                for(int j = 0; j< this.subRooms[i].GetSubRooms().Count; j++)
+                {
+                    rooms.Add(this.subRooms[i].GetSubRooms()[j]);
+                }
+            }
+            return rooms;
+        }
+    }
+
+    public override bool Equals(object obj)
+    {
+        LegacyRoom aux = obj as LegacyRoom;
+
+        return aux!=null && this.generationString == aux.generationString;
+    }
+
+}
--- Assets/Code/Room.cs
+++ Assets/Code/Room.cs
@@ -1,29 +1,87 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Room {
 
+    private PositiveVector2 startPoint, roomSize;
 
-    //Starting point from left-down corner
-    private Vector3 startPoint;
-    private Vector3 roomSize;
-    private int numberOfRooms;
-    private float roomPercentage = 0.975f;
-    private List<Room> subRooms;
-    private int numberCorridors = 0;
-    //First generation, first number, second, second... 
-    private string generationString;
-    public Vector3 StartPoint
+    private List<Room> roomChilds;
+
+
+    public Room(PositiveVector2 startPoint, PositiveVector2 size, PositiveVector2 minSize, int roomsAsigned)
+    {
+        //En cada habitación, parto en dos si me deja el minimo de tamaño. 
+        this.startPoint = startPoint;
+        this.roomSize = size;
+
+        if (!CheckIfMinSize(minSize))
+        {
+            if (roomsAsigned > 1)
+            {
+                roomChilds = new List<Room>(BSPDungeon.Instance.MakeRooms(Random.Range(0, 100) < 50, size, roomsAsigned, startPoint));
+                //Debug.Log("Room : " + this.ToString() + " childs: " + roomChilds[0].ToString() + "  ::::::   " + roomChilds[1].ToString());
+                foreach (Room room in roomChilds)
+                {
+                    if (room.CheckIfMinSize(minSize))
+                    {
+                        BSPDungeon.Instance.AddRoom(this);
+                        foreach(Room child in roomChilds)
+                        {
+                            BSPDungeon.Instance.DeleteRoom(child); 
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                BSPDungeon.Instance.AddRoom(this); 
+            }
+        }
+        
+
+
+    }
+
+    public void SetDefinitiveSize(float mod, PositiveVector2 minSize)
+    {
+
+        PositiveVector2 oldSize = roomSize; 
+        roomSize = new PositiveVector2((int)(roomSize.X * mod), (int)(roomSize.Y*mod));
+        if (CheckIfMinSize(minSize))
+        {
+            roomSize = oldSize; 
+        }
+       
+    }
+
+
+    public bool CheckIfMinSize(PositiveVector2 minSize)
+    {
+        return (this.roomSize.X < minSize.X || this.roomSize.Y < minSize.Y);
+    }
+
+    public override string ToString()
+    {
+        return "Point Start: (" + startPoint.X + "," + startPoint.Y + ")  Size: (" + roomSize.X + "," + roomSize.Y + ")"; 
+    }
+
+
+
+
+
+
+
+    public PositiveVector2 StartPoint
     {
         get
         {
             return startPoint;
         }
-
     }
 
-    public Vector3 RoomSize
+    public PositiveVector2 RoomSize
     {
         get
         {
@@ -31,129 +89,4 @@ public class Room {
         }
 
     }
-
-    public List<Room> SubRooms
-    {
-        get
-        {
-            return subRooms;
-        }
-
-       
-    }
-
-    public int NumberCorridors
-    {
-        get
-        {
-            return numberCorridors;
-        }
-
-        set
-        {
-            numberCorridors = value;
-        }
-    }
-
-    public string GenerationString
-    {
-        get
-        {
-            return generationString;
-        }
-
-    }
-
-    public Room (Vector3 startPoint, Vector3 roomSize, int roomDivisions, float roomPercentage, string previousGeneration = "")
-    {
-        this.startPoint = startPoint;
-        this.roomSize = roomSize;
-        this.numberOfRooms = roomDivisions;
-        this.roomPercentage = roomPercentage;
-        this.generationString = previousGeneration;
-        if (this.numberOfRooms == 1)
-        {
-            this.startPoint = this.startPoint * (1 + (1 - roomPercentage));
-            this.roomSize = this.roomSize * roomPercentage;
-
-        }
-        else if (this.numberOfRooms > 1)
-        {
-            this.subRooms = new List<Room>();
-            bool twoD = this.roomSize.z == 0;
-
-            //Re-escalate the room for lesser size. 
-            Vector3 trueStartPoint = new Vector3(this.startPoint.x + 15, twoD ? this.startPoint.y + 15 : 0, twoD ? 0 : this.startPoint.z +15);
-            Vector3 trueRoomSize = new Vector3(this.roomSize.x -15, twoD ? this.roomSize.y -15 : 0, twoD?0:this.roomSize.z -15); //* this.roomPercentage;
-
-            bool horizontal = Random.Range(0, 100) < 50;
-            int firstRooms;
-            if (numberOfRooms > 2)
-            {
-                firstRooms = (int)Random.Range(this.numberOfRooms * 0.4f, this.numberOfRooms * 0.6f);
-            }
-            else
-            {
-                firstRooms = 1;
-            }
-            float sizeFirstRoom;
-            if (horizontal)
-            {
-                
-                sizeFirstRoom = twoD ? trueRoomSize.y : trueRoomSize.z;
-                sizeFirstRoom = Random.Range(sizeFirstRoom * 0.4f, sizeFirstRoom * 0.6f) ;
-                Room room = new Room(trueStartPoint, new Vector3(trueRoomSize.x, twoD ? sizeFirstRoom : 0, twoD ? 0 : sizeFirstRoom), firstRooms, roomPercentage, this.generationString+"0");
-                this.subRooms.Add(room);
-                if ((this.numberOfRooms - firstRooms) != 0)
-                {
-                    room = new Room(trueStartPoint + (twoD ? Vector3.up * (sizeFirstRoom): Vector3.forward * (sizeFirstRoom)),
-                    new Vector3(trueRoomSize.x, twoD ? trueRoomSize.y - sizeFirstRoom : 0, twoD ? 0 : trueRoomSize.z - sizeFirstRoom), this.numberOfRooms - firstRooms, roomPercentage, this.generationString+"1");
-                    this.subRooms.Add(room);
-                }
-            }
-            else
-            {
-                
-                sizeFirstRoom = Random.Range(trueRoomSize.x * 0.4f, trueRoomSize.x * 0.6f);
-                Room room = new Room(trueStartPoint, new Vector3(sizeFirstRoom, twoD ? trueRoomSize.y : 0, twoD ? 0 : trueRoomSize.z), firstRooms, roomPercentage, this.generationString + "0");
-                this.subRooms.Add(room);
-                if ((this.numberOfRooms - firstRooms) != 0)
-                {
-                    room = new Room(trueStartPoint + Vector3.right * (sizeFirstRoom),
-                        new Vector3(trueRoomSize.x - sizeFirstRoom, twoD ? trueRoomSize.y : 0, twoD ? 0 : trueRoomSize.z), this.numberOfRooms - firstRooms, roomPercentage, this.generationString +"1");
-                    this.subRooms.Add(room);
-                }
-            }
-
-        }
-    }
-
-    public List<Room> GetSubRooms()
-    {
-        List<Room> rooms = new List<Room>();
-        if (this.subRooms == null)
-        {
-            rooms.Add(this);
-            return rooms;
-        }
-        else
-        {
-            for (int i = 0; i < this.subRooms.Count; i++)
-            {
-                for(int j = 0; j< this.subRooms[i].GetSubRooms().Count; j++)
-                {
-                    rooms.Add(this.subRooms[i].GetSubRooms()[j]);
-                }
-            }
-            return rooms;
-        }
-    }
-
-    public override bool Equals(object obj)
-    {
-        Room aux = obj as Room;
-
-        return aux!=null && this.generationString == aux.generationString;
-    }
-
 }
